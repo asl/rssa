550d5d31457f74b385dd19dd7eab51db14ea0614
Anton Korobeynikov <asl@math.spbu.ru>
2008-11-30 03:46:18 +0300

    Drop non-lazy implementation: proof-of-concept is mature enough.

--- R/ssa.R
+++ R/ssa.R
@@ -17,13 +17,47 @@
 #   Free Software Foundation, Inc., 675 Mass Ave, Cambridge, 
 #   MA 02139, USA.
 
+new.ssa <- function(x,
+                    L = (N - 1) %/% 2,
+                    ..., 
+                    method = c("hankel", "toeplitz"),
+                    centering = c("none", "row", "both"),
+                    force.decompose = TRUE) {
+  method <- match.arg(method);
+  centering <- match.arg(centering);
+  N <- length(x);
+
+  # Create information body
+  this <- list(length = N,
+               window = L,
+               call = match.call(),
+               series = deparse(substitute(x)),
+               method = method,
+               centering = centering);
+
+  # Create data storage
+  attr(this, ".env") <- new.env();
+
+  # Save series
+  .set.ssa(this, "F", x);
+  
+  # Make this S3 object
+  class(this) <- "ssa";
+
+  # Decompose, if necessary
+  if (force.decompose)
+    decompose(this, ...);
+
+  this;
+}
+
 # FIXME: add version, which accepts first column and last row
 hankel <- function(X, L) {
   if (is.matrix(X) && nargs() == 1) {
      L <- dim(X)[1]; K <- dim(X)[2]; N <- K + L - 1;
      left  <- c(1:L, L*(2:K));
      right <- c(1+L*(0:(K-1)), ((K-1)*L+2):(K*L));
-     v <- sapply(1:N, function(i) mean(X[seq(left[i], right[i], by = L-1)]));
+     v <- sapply(1:N, function(i) mean(X[seq.int(left[i], right[i], by = L-1)]));
      return (v);
   }
 
@@ -37,102 +71,213 @@ hankel <- function(X, L) {
   outer(1:L, 1:K, function(x,y) X[x+y-1]);
 }
 
-ssa.decompose <- function(x,
-                          L = (N - 1) %/% 2,
-                          method = c("hankel", "toeplitz"),
-                          centering = c("none", "row", "both")) {
-  method <- match.arg(method);
-  centering <- match.arg(centering);
-  N <- length(x); K <- N - L + 1;
-  .norm <- function(X) sqrt(sum(X^2));
+.decompose.ssa.hankel <- function(this,
+                                  nu = min(L, K), nv = min(L, K)) {
+  N <- this$length; L <- this$window; K <- N - L + 1;
+  F <- .get.ssa(this, "F");
 
-  X <- hankel(x, L = L);
-  cl <- cU <- cV <- NULL;
+  X <- hankel(F, L = L);
+
+  # FIXME: Use special SVD for hankel matrixes
+  S <- svd(X, nu = nu, nv = nv);
+
+  # Save results
+  .set.ssa(this, "lambda", S$d);
+  if (!is.null(S$u))
+    .set.ssa(this, "U", S$u);
+  if (!is.null(S$v))
+    .set.ssa(this, "V", S$v);
+}
+
+.decompose.ssa.toeplitz <- function(this, ...) {
+  stop("Unimplemented!")
+}
+
+decompose.ssa <- function(this, ...) {
+  method <- this$method;
+
   if (identical(method, "hankel")) {
-    # Perform extra centering, if needed
-    rmeans <- cmeans <- NULL;
-    if (identical(centering, "row") || identical(centering, "both")) {
-      rmeans <- rowMeans(X);
-      X <- sweep(X, 1, rmeans);
-    }
-    if (identical(centering, "both")) {
-      cmeans <- colMeans(X);
-      X <- sweep(X, 2, cmeans);
-    }
-
-    # Compute eigentriples for first and second means
-    if (!is.null(rmeans)) {
-      mnorm <- .norm(rmeans);
-      cl <- sqrt(mnorm * sqrt(K));
-      cU <- rmeans / mnorm;
-      cV <- rep(1, K) * mnorm / cl;
-    }
-    if (!is.null(cmeans)) {
-      mnorm <- .norm(cmeans);
-      cl <- c(cl, sqrt(mnorm * sqrt(L)));
-      cU <- cbind(cU, rep(1, L) / sqrt(L));
-      cV <- cbind(cV, sqrt(L) * cmeans / cl[2]);
-    }
-
-    # FIXME: Use special SVD for hankel matrixes
-    S <- svd(X);
-    names(S) <- c("lambda", "U", "V");
+    .decompose.ssa.hankel(this, ...);
   } else if (identical(method, "toeplitz")) {
-    R <- acf(x, lag.max = L - 1, type = "covariance", plot = FALSE, demean = FALSE);
-    # FIXME: find a better way to construct toeplitz matrix
-    C <- toeplitz(as.vector(R$acf));
-    S <- eigen(C, symmetric = TRUE);
-    names(S) <- c("lambda", "U");
-    # This is pretty inefficient, find a better way
-    S$U <- S$U %*% diag(1/apply(S$U, 2, .norm));
-    Z <- t(X) %*% S$U;
-    S$lambda <- (apply(Z, 2, .norm))^2;
-    S$V <- Z %*% diag(1/S$lambda, nrow = L);
+    .decompose.ssa.toeplitz(this, ...);
   } else {
     stop("Unknown method in SSA")
   }
-
-
-  # Add centering eigentriples, if any
-  if (!is.null(cl)) {
-    S$lambda <- c(cl, S$lambda);
-    S$U <- cbind(cU, S$U);
-    S$V <- cbind(cV, S$V);
-  }
-
-  S$call   <- match.call();
-  S$method <- method;
-  S$centering <- centering;
-  S$series <- deparse(substitute(x));
-  S$window <- L;
-  S$length <- length(x);
-  class(S) <- "ssa";
-
-  return (S);
 }
 
-ssa.reconstruct <- function(S, groups) {
+# C call hankelize_one() is equivalent to the following R code:
+# hankel(outer(U, V))
+.hankelize.one <- function(U, V) {
+  L <- length(U); K <- length(V); N <- K + L - 1;
+  F <- rep(0, N);
+  .C("hankelize_one",
+     as.double(F),
+     as.double(U), as.double(V),
+     as.integer(L), as.integer(K))[[1]];
+}
+
+precache.ssa <- function(this, n, ...) {
+  if (missing(n)) {
+    warning("Amount of sub-series missed, precaching EVERYTHING",
+            immediate. = TRUE);
+    n <- nlambda(this);
+  }
+
+  # Calculate numbers of sub-series to be calculated
+  if (.exists.ssa(this, "cache:series")) {
+    info <- .get.ssa(this, "cache:series");
+  } else {
+    info <- numeric(0);
+  }
+  new <- setdiff(1:n, info);
+
+  # We're supporting only 'full' data for now
+  U <- .get.ssa(this, "U");
+  V <- .get.ssa(this, "V");
+  lambda <- .get.ssa(this, "lambda");
+
+  new <- sapply(new,
+                function(i) {
+                  .cache(this,
+                         lambda[i] * .hankelize.one(U[,i], V[,i]),
+                         i)});
+  .set.ssa(this, "cache:series", union(info, new));
+}
+
+.cache <- function(this, F, index) {
+  name <- paste("cache:", index, sep = "");
+  .set.ssa(this, name, F);
+  index;
+}
+
+cleanup.ssa <- function(this, ...) {
+  .remove.ssa(this, ls(.storage.ssa(this), pattern = "cache:"));
+  gc();
+}
+
+reconstruct.ssa <- function(this, groups, ..., cache = TRUE) {
   out <- list();
+  nu <- nu(this); nv <- nv(this);
 
   if (missing(groups))
-    groups <- as.list(1:length(S$lambda));
+    groups <- as.list(1:nlambda(this));
+
+  # We're supporting only 'full' data for now
+  U <- .get.ssa(this, "U");
+  V <- .get.ssa(this, "V");
+  lambda <- .get.ssa(this, "lambda");
 
   for (i in seq_along(groups)) {
     group <- groups[[i]];
-    X <- S$U[, group] %*% diag(S$lambda[group], nrow = length(group)) %*% t(S$V[,group])
-    out[[i]] <- hankel(X);
+
+    out[[i]] <- hankel(U[, group] %*%
+                       diag(lambda[group], nrow = length(group)) %*%
+                       t(V[,group]));
   }
+
   names(out) <- paste("F", 1:length(groups), sep="");
-  return (out);
+
+  # Reconstructed series can be pretty huge...
+  invisible(out);
 }
 
-#.F <- function(x) exp(-.01 * x)*cos(x/100);
-#F <- .F(1:100);
-#a = 1; b = 0; T = 10;
-#.F <- function(x) a*x + b + 5*sin(2*pi*x/T);
-#F <- .F(1:19);
-#Rprof();
-#SS <- ssa.decompose(F, centering = "both");
-#L <- ssa.reconstruct(SS, list(1:2));
+nu.ssa <- function(this, ...) {
+  ifelse(.exists.ssa(this, "U"), dim(.get.ssa(this, "U"))[2], 0);
+}
+
+nv.ssa <- function(this, ...) {
+  ifelse(.exists.ssa(this, "V"), dim(.get.ssa(this, "V"))[2], 0);
+}
+
+nlambda.ssa <- function(this, ...) {
+  ifelse(.exists.ssa(this, "lambda"), length(.get.ssa(this, "lambda")), 0);
+}
+
+clone.ssa <- function(this, ...) {
+  # Copy the information body
+  obj <- this;
+
+  # Make new storage
+  clone.env <- new.env();
+  this.env <- .storage.ssa(this);
+  attr(obj, ".env") <- clone.env;
+
+  # Copy the contents of data storage
+  for (field in ls(envir = this.env, all.names = TRUE)) {
+    value <- get(field, envir = this.env, inherits = FALSE);
+    assign(field, value, envir = clone.env, inherits = FALSE);
+  }
+
+  obj;
+}
+
+.storage.ssa <- function(this) {
+  attr(this, ".env");
+}
+
+.get.ssa <- function(this, name) {
+  get(name, envir = .storage.ssa(this));
+}
+
+.set.ssa <- function(this, name, value) {
+  assign(name, value, envir = .storage.ssa(this), inherits = FALSE);
+}
+
+.exists.ssa <- function(this, name) {
+  exists(name, envir = .storage.ssa(this), inherits = FALSE);
+}
+
+.remove.ssa <- function(this, name) {
+  rm(list = name, envir = .storage.ssa(this), inherits = FALSE);
+}
+
+'$.ssa' <- function(this, name) {
+  if (ind <- charmatch(name, names(this), nomatch = 0))
+    return (this[[ind]]);
+
+  if (.exists.ssa(this, name))
+    return (.get.ssa(this, name));
+
+  NULL;
+}
+
+clone <- function(this, ...) {
+  UseMethod("clone");
+}
+
+decompose <- function(this, ...) {
+  UseMethod("decompose");
+}
+
+reconstruct <- function(this, ...) {
+  UseMethod("reconstruct");
+}
+
+nu <- function(this, ...) {
+  UseMethod("nu");
+}
+
+nv <- function(this, ...) {
+  UseMethod("nv");
+}
+
+nlambda <- function(this, ...) {
+  UseMethod("nlambda");
+}
+
+precache <- function(this, ...) {
+  UseMethod("precache");
+}
+
+cleanup <- function(this, ...) {
+  UseMethod("cleanup");
+}
+
+.F <- function(x) exp(-.01 * x)*cos(x/100) + 0.05*rnorm(length(x));
+ F <- .F(1:500);
+
+#Rprof(memory = "both");
+#s <- new.ssa(F);
+#precache(s);
 #Rprof(NULL);
-#summaryRprof();
+#summaryRprof(memory = "both");
