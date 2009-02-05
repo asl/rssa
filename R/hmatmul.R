d7b7b9ee524736bc75ce2950c22aa4fad0da3831
Anton Korobeynikov <asl@math.spbu.ru>
2009-02-06 02:07:20 +0300

    Add proof-of-concept hankel matrix-vector multiplication code

---
+++ R/hmatmul.R
@@ -0,0 +1,51 @@
+#   R package for Singular Spectrum Analysis
+#   Copyright (c) 2008 Anton Korobeynikov <asl@math.spbu.ru>
+#   
+#   This program is free software; you can redistribute it 
+#   and/or modify it under the terms of the GNU General Public 
+#   License as published by the Free Software Foundation; 
+#   either version 2 of the License, or (at your option) 
+#   any later version.
+#
+#   This program is distributed in the hope that it will be 
+#   useful, but WITHOUT ANY WARRANTY; without even the implied 
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+#   PURPOSE.  See the GNU General Public License for more details.
+#   
+#   You should have received a copy of the GNU General Public 
+#   License along with this program; if not, write to the 
+#   Free Software Foundation, Inc., 675 Mass Ave, Cambridge, 
+#   MA 02139, USA.
+
+tcirc <- function(F, L = (N - 1) %/% 2) {
+  N <- length(F); K = N - L + 1;
+  .res <- list();
+  .res$C <- as.vector(fft(c(F[K:N], F[1:(K-1)])));
+  .res$L <- L;
+  return (.res);
+}
+
+hmul <- function(C, v) {
+  v <- as.vector(fft(C$C * fft(c(rev(v), rep(0, C$L-1))), inverse = TRUE));
+  Re((v/length(C$C))[1:C$L]);
+}
+
+mes <- function(N = 1000, L = (N %/% 2), n = 50) {
+  F <- rnorm(N);
+  v <- rnorm(N - L + 1);
+  C <- tcirc(F, L = L);
+  X <- hankel(F, L = L);
+  st1 <- system.time(for (i in 1:n) X %*% v);
+  st2 <- system.time(for (i in 1:n) hmul(C, v));
+  c(st1[["user.self"]], st2[["user.self"]]);
+}
+
+
+#Rprof();
+#for (i in 1:250) {
+#  r1 <- X %*% v;
+#  r2 <- hmul(C, v);
+#}
+#Rprof(NULL);
+#print(max(abs(r1-r2)));
+#summaryRprof();
