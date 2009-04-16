2cb57649621e004abc68684516ca1dd7fc9ff5b3
Anton Korobeynikov <asl@math.spbu.ru>
2009-04-16 21:34:59 +0400

    Reduce the computation burden of fft hankelization (thanks, Kostya!) Move all hankel stuff to proper places

--- src/hankel.c
+++ src/hankel.c
@@ -35,24 +35,24 @@ typedef struct {
   R_len_t length;
 } hankel_matrix;
 
-unsigned _hankel_nrow(const void *matrix) {
+static unsigned hankel_nrow(const void *matrix) {
   const hankel_matrix *h = matrix;
   return h->window;
 }
 
-unsigned _hankel_ncol(const void *matrix) {
+static unsigned hankel_ncol(const void *matrix) {
   const hankel_matrix *h = matrix;
   return h->length - h->window + 1;
 }
 
-void _free_circulant(hankel_matrix *h) {
+static void free_circulant(hankel_matrix *h) {
   fftw_free(h->circ_freq);
   fftw_destroy_plan(h->r2c_plan);
   fftw_destroy_plan(h->c2r_plan);
 }
 
-void _initialize_circulant(hankel_matrix *h,
-                           const double *F, R_len_t N, R_len_t L) {
+static void initialize_circulant(hankel_matrix *h,
+                                 const double *F, R_len_t N, R_len_t L) {
   R_len_t K = N - L + 1, i;
   fftw_complex *ocirc;
   fftw_plan p1, p2;
@@ -87,9 +87,9 @@ void _initialize_circulant(hankel_matrix *h,
   h->length = N;
 }
 
-void _hankel_matmul(double* out,
-                    const double* v,
-                    const void* matrix) {
+static void hankel_matmul(double* out,
+                          const double* v,
+                          const void* matrix) {
   const hankel_matrix *h = matrix;
   R_len_t N = h->length, L = h->window;
   R_len_t K = N - L + 1, i;
@@ -123,9 +123,9 @@ void _hankel_matmul(double* out,
   fftw_free(ocirc);
 }
 
-void _hankel_tmatmul(double* out,
-                     const double* v,
-                     const void* matrix) {
+static void hankel_tmatmul(double* out,
+                           const double* v,
+                           const void* matrix) {
   const hankel_matrix *h = matrix;
   R_len_t N = h->length, L = h->window;
   R_len_t K = N - L + 1, i;
@@ -159,7 +159,98 @@ void _hankel_tmatmul(double* out,
   fftw_free(ocirc);
 }
 
-static void hmatFinalizer(SEXP ptr) {
+/* This is just direct R-to-C translation  */
+static R_INLINE void hankelize(double *F,
+                               double *U, double *V,
+                               R_len_t L, R_len_t K) {
+  R_len_t i, N = K + L - 1;
+  R_len_t leftu, rightu, leftv, rightv, l, j;
+
+  for (i = 0; i < N; ++i) {
+    double s = 0;
+
+    if (i < L) {
+      leftu = i;
+      leftv = 0;
+    } else {
+      leftu = L - 1;
+      leftv = i - L + 1;
+    }
+    if (i < K) {
+      rightu = 0;
+      rightv = i;
+    } else {
+      rightu = i - K + 1;
+      rightv = K - 1;
+    }
+
+    l = leftu - rightu + 1;
+
+    for (j = 0; j < l; ++j) {
+      s += U[leftu - j] * V[leftv + j];
+    }
+
+    F[i] = s / (double) l;
+  }
+}
+
+static R_INLINE void hankelize_fft(double *F,
+                                   double *U, double *V,
+                                   size_t L, size_t K) {
+  size_t i, N = K + L - 1;
+  double *iU, *iV;
+  fftw_complex *cU, *cV;
+  fftw_plan p1, p2;
+
+  /* Allocate needed memory */
+  iU = (double*) fftw_malloc(N * sizeof(double));
+  iV = (double*) fftw_malloc(N * sizeof(double));
+  cU = (fftw_complex*) fftw_malloc((N/2 + 1) * sizeof(fftw_complex));
+  cV = (fftw_complex*) fftw_malloc((N/2 + 1) * sizeof(fftw_complex));
+
+  /* Estimate the best plans for given input length */
+  p1 = fftw_plan_dft_r2c_1d(N, iU, cU, FFTW_ESTIMATE);
+  p2 = fftw_plan_dft_c2r_1d(N, cU, iU, FFTW_ESTIMATE);
+
+  /* Fill in buffers */
+  memcpy(iU, U, L*sizeof(double));
+  memset(iU+L, 0, (K - 1)*sizeof(double));
+
+  memcpy(iV, V, K*sizeof(double));
+  memset(iV+K, 0, (L - 1)*sizeof(double));
+
+  /* Compute the FFTs */
+  fftw_execute_dft_r2c(p1, iU, cU);
+  fftw_execute_dft_r2c(p1, iV, cV);
+
+  /* Dot-multiply */
+  for (i = 0; i < N/2 + 1; ++i)
+    cU[i] = cU[i] * cV[i];
+
+  /* Compute the inverse FFT */
+  fftw_execute_dft_c2r(p2, cU, iU);
+
+  /* Form the result */
+  for (i = 0; i < N; ++i) {
+    size_t leftu, rightu, l;
+
+    if (i < L) leftu = i; else leftu = L - 1;
+    if (i < K) rightu = 0; else  rightu = i - K + 1;
+
+    l = N*(leftu - rightu + 1);
+
+    F[i] = iU[i] / l;
+  }
+
+  fftw_free(iU);
+  fftw_free(iV);
+  fftw_free(cU);
+  fftw_free(cV);
+  fftw_destroy_plan(p1);
+  fftw_destroy_plan(p2);
+}
+
+static void hmat_finalizer(SEXP ptr) {
   ext_matrix *e;
   hankel_matrix *h;
 
@@ -175,7 +266,7 @@ static void hmatFinalizer(SEXP ptr) {
 
   h = e->matrix;
 
-  _free_circulant(h);
+  free_circulant(h);
   Free(h);
 
   Free(e);
@@ -194,19 +285,19 @@ SEXP initialize_hmat(SEXP F, SEXP window) {
   /* Allocate memory */
   e = Calloc(1, ext_matrix);
   e->type = "hankel matrix";
-  e->mulfn = _hankel_matmul;
-  e->tmulfn = _hankel_tmatmul;
-  e->ncol = _hankel_ncol;
-  e->nrow = _hankel_nrow;
+  e->mulfn = hankel_matmul;
+  e->tmulfn = hankel_tmatmul;
+  e->ncol = hankel_ncol;
+  e->nrow = hankel_nrow;
 
   /* Build toeplitz circulants for hankel matrix */
   h = Calloc(1, hankel_matrix);
-  _initialize_circulant(h, REAL(F), N, L);
+  initialize_circulant(h, REAL(F), N, L);
   e->matrix = h;
 
-  /* Make an external pointer envolope */
+  /* Make an external pointer envelope */
   hmat = R_MakeExternalPtr(e, install("external matrix"), R_NilValue);
-  R_RegisterCFinalizer(hmat, hmatFinalizer);
+  R_RegisterCFinalizer(hmat, hmat_finalizer);
 
   return hmat;
 }
@@ -255,7 +346,7 @@ SEXP hankel_rows(SEXP ptr) {
     ext_matrix *e = R_ExternalPtrAddr(ptr);
 
     PROTECT(ans = allocVector(INTSXP, 1));
-    INTEGER(ans)[0] = _hankel_nrow(e->matrix);
+    INTEGER(ans)[0] = hankel_nrow(e->matrix);
     UNPROTECT(1);
   } else
     error("pointer provided is not a hankel matrix");
@@ -276,7 +367,7 @@ SEXP hankel_cols(SEXP ptr) {
     ext_matrix *e = R_ExternalPtrAddr(ptr);
 
     PROTECT(ans = allocVector(INTSXP, 1));
-    INTEGER(ans)[0] = _hankel_ncol(e->matrix);
+    INTEGER(ans)[0] = hankel_ncol(e->matrix);
     UNPROTECT(1);
   } else
     error("pointer provided is not a hankel matrix");
@@ -313,9 +404,9 @@ SEXP hmatmul(SEXP hmat, SEXP v, SEXP transposed) {
 
     /* Calculate the product */
     if (LOGICAL(transposed)[0])
-      _hankel_tmatmul(REAL(Y), REAL(v), h);
+      hankel_tmatmul(REAL(Y), REAL(v), h);
     else
-      _hankel_matmul(REAL(Y), REAL(v), h);
+      hankel_matmul(REAL(Y), REAL(v), h);
 
     UNPROTECT(1);
   } else
@@ -324,4 +415,71 @@ SEXP hmatmul(SEXP hmat, SEXP v, SEXP transposed) {
   UNPROTECT(1);
 
   return Y;
+}
+
+SEXP hankelize_one(SEXP U, SEXP V) {
+  double *rU = REAL(U), *rV = REAL(V), *rF;
+  R_len_t L, K;
+  SEXP F;
+
+  /* Calculate length of inputs */
+  L = length(U); K = length(V);
+
+  /* Allocate buffer for output */
+  PROTECT(F = allocVector(REALSXP, K + L - 1));
+  rF = REAL(F);
+
+  /* Perform the actual hankelization */
+  hankelize(rF, rU, rV, L, K);
+
+  UNPROTECT(1);
+  return F;
+}
+
+SEXP hankelize_one_fft(SEXP U, SEXP V) {
+  double *rU = REAL(U), *rV = REAL(V), *rF;
+  R_len_t L, K;
+  SEXP F;
+
+  /* Calculate length of inputs */
+  L = length(U); K = length(V);
+
+  /* Allocate buffer for output */
+  PROTECT(F = allocVector(REALSXP, K + L - 1));
+  rF = REAL(F);
+
+  /* Perform the actual hankelization */
+  hankelize_fft(rF, rU, rV, L, K);
+
+  UNPROTECT(1);
+  return F;
+}
+
+SEXP hankelize_multi(SEXP U, SEXP V) {
+  double *rU = REAL(U), *rV = REAL(V), *rF;
+  R_len_t L, K, N, i, count;
+  SEXP F;
+  int *dimu, *dimv;
+
+  /* Calculate length of inputs and output */
+  dimu = INTEGER(getAttrib(U, R_DimSymbol));
+  dimv = INTEGER(getAttrib(V, R_DimSymbol));
+  L = dimu[0]; K = dimv[0];
+  if ((count = dimu[1]) != dimv[1])
+    error("Both 'U' and 'V' should have equal number of columns");
+  N = K + L - 1;
+
+  /* Allocate buffer for output */
+  PROTECT(F = allocMatrix(REALSXP, N, count));
+  rF = REAL(F);
+
+  /* Perform the actual hankelization */
+  for (i = 0; i < count; ++i) {
+    R_CheckUserInterrupt();
+    /* TODO: nice target for OpenMP stuff */
+    hankelize(rF+i*N, rU+i*L, rV+i*K, L, K);
+  }
+
+  UNPROTECT(1);
+  return F;
 }
