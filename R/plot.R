264d71e6044a31eaa9dc193462f68dbc88ea3f1f
Alexander Shlemov <shlemovalex@gmail.com>
2013-04-24 05:14:48 +0200

    Add 'ref' and 'symmetric' pars

--- R/plot.R
+++ R/plot.R
@@ -18,18 +18,31 @@
 #   MA 02139, USA.
 
 
-prepanel.eigenvectors <- function(x, y, ssaobj) {
+prepanel.eigenvectors <- function(x, y, ssaobj, symmetric = FALSE) {
   V <- ssaobj$U[,y];
   U <- if (identical(x, y)) 1:length(V)
        else ssaobj$U[,x]
 
-  prepanel.default.xyplot(U, V);
+  res <- prepanel.default.xyplot(U, V);
+  if (symmetric) {
+    res$ylim <- range(V, -V);
+    if (!identical(x, y))
+      res$xlim <- range(U, -U);
+  }
+
+  res;
 }
 
-panel.eigenvectors <- function(x, y, ssaobj, ...) {
+panel.eigenvectors <- function(x, y, ssaobj, ..., ref = FALSE) {
   V <- ssaobj$U[,y];
   U <- if (identical(x, y)) 1:length(V)
        else ssaobj$U[,x]
+
+  if (ref) {
+    panel.abline(h = 0, ..., reference = TRUE)
+    if (!identical(x, y))
+      panel.abline(v = 0, ..., reference = TRUE)
+  }
 
   panel.xyplot(U, V, ...);
 }
