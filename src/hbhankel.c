b80a0a3c125a8b3e1d78e89d2663fb1c637f0c36
Alexander Shlemov <shlemovalex@gmail.com>
2013-08-15 02:16:01 +0400

    Beautify

--- src/hbhankel.c
+++ src/hbhankel.c
@@ -160,7 +160,7 @@ static void hbhankel_matmul(double* out,
         circ[i + j*Nx] = v[Kx*Ky - i - j*Kx - 1];
   } else {
     for (i = 0; i < h->col_ind->num; ++i) {
-      circ[(Kx-1) + (Ky-1)*Nx - h->col_ind->ind[i]] = v[i];
+      circ[(Kx - 1) + (Ky - 1)*Nx - h->col_ind->ind[i]] = v[i];
     }
   }
 
@@ -208,7 +208,7 @@ static void hbhankel_tmatmul(double* out,
         circ[(i + Kx - 1) + (j + Ky - 1)*Nx] = v[Lx*Ly - i - j*Lx - 1];
   } else {
     for (i = 0; i < h->row_ind->num; ++i) {
-      circ[Nx*Ny-1-h->row_ind->ind[i]] = v[i];
+      circ[Nx*Ny - 1 - h->row_ind->ind[i]] = v[i];
     }
   }
 
@@ -310,12 +310,9 @@ static void hbhankel_matmul(double* out,
 }
 
 static void hbhankel_tmatmul(double* out,
-                            const double* v,
-                            const void* matrix) {
-  error("FFTW-less version of 2D-SSA is not implemented yet!");
-}
-
-static R_INLINE void hbhankelize_fft(double *F,
+                             const double* v,
+                             const void* matrix) {
+  error("FFTW-less version of 2D-SSA istatic R_INLINE void hbhankelize_fft(double *F,
                                      const double *U, const double *V,
                                      const hbhankel_matrix* h) {
   error("FFTW-less version of 2D-SSA is not implemented yet!");
