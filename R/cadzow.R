92f7302de3c4206af6b06663b2327e4952d0bb1c
Alexander Shlemov <shlemovalex@gmail.com>
2014-01-26 03:35:47 +0400

    Add format conversion generic

--- R/cadzow.R
+++ R/cadzow.R
@@ -39,11 +39,31 @@
   sum(weights * F1 * F2)
 }
 
+.inner.fmt.conversion <- function(x, ...)
+  UseMethod(".inner.fmt.conversion")
+
+.inner.fmt.conversion.ssa <- function(x, ...)
+  identical
+
+.inner.fmt.conversion.1d.ssa <- .inner.fmt.conversion.toeplitz.ssa <- function(x, ...)
+  as.numeric
+
+.inner.fmt.conversion.cssa <- function(x, ...)
+  as.complex
+
+.inner.fmt.conversion.2d.ssa <- function(x, ...)
+  as.matrix
+
+.inner.fmt.conversion.mssa <- function(x, ...)
+  .to.series.list
 
 cadzow.ssa <- function(x, rank,
                        correct = TRUE,
                        eps = 1e-6, numiter = 0,
                        ..., cache = TRUE) {
+  # Get conversion
+  conversion <- .inner.fmt.conversion(x)
+
   # Get weights and mask
   weights <- .hweights(x)
   mask <- weights > 0
@@ -58,7 +78,7 @@ cadzow.ssa <- function(x, rank,
   it <- 0
   repeat {
     s <- clone(x, copy.cache = FALSE, copy.storage = FALSE)
-    .set(s, "F", F)
+    .set(s, "F", conversion(F))
     r <- reconstruct(s, groups = list(1:rank), ..., cache = cache)
     stopifnot(length(r) == 1)
     rF <- r[[1]]
