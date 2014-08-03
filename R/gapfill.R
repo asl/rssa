c836603a6f9a0bbb86d0302b4658bc09bf63b48e
Anton Korobeynikov <anton@korobeynikov.info>
2014-08-04 01:55:01 +0400

    Calculate the borders properly

--- R/gapfill.R
+++ R/gapfill.R
@@ -81,11 +81,11 @@ pi.project.missing <- function(U, v) {
 
 classify.gaps <- function(na.idx, L, N) {
   ## First, split the na.idx into separate clusters
-  d <- which(diff(na.idx) > L)
-  cl.borders <- if (length(na.idx) == 1) c(na.idx, na.idx) else sort(unique(c(min(na.idx), na.idx[d], na.idx[d+1], max(na.idx))))
-  stopifnot(length(cl.borders) %% 2 == 0)
-  left <- cl.borders[c(TRUE, FALSE)]
-  right <- cl.borders[c(FALSE, TRUE)]
+  left  <- na.idx[which(diff(c(-L, na.idx)) > L)]
+  right <- na.idx[which(diff(c(na.idx, N+L)) > L)]
+
+  stopifnot(length(left) == length(right))
+  stopifnot(all(left <= right))
 
   # Iterate over borders classifying the gaps
   res <- list()
