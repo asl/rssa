88a705d59b4ac7a95693f555dda21e0b28f564af
Alexander Shlemov <shlemovalex@gmail.com>
2014-08-29 12:42:01 +0400

    Add eigenplot for nD-SSA

--- R/plotn.R
+++ R/plotn.R
@@ -86,3 +86,73 @@ plot.nd.ssa.reconstruction <- function(x, slice, ...) {
   else
     stop("Cannot display array of rank higher than 2. Use `slice' argument")
 }
+
+.plot.ssa.vectors.nd.ssa <- function(x, slice, ...,
+                                    what = c("eigen", "factor"),
+                                    plot.contrib = FALSE,
+                                    idx) {
+  what <- match.arg(what)
+
+  dots <- list(...)
+
+  if (max(idx) > nsigma(x))
+    stop("Too few eigentriples computed for this decomposition")
+
+  N <- x$length
+  L <- x$window
+  K <- ifelse(x$circular, N, N - L + 1)
+
+  if (identical(what, "eigen")) {
+    mask <- x$wmask
+    dimension <- L
+    vmatrix <- .U(x)[, idx, drop = FALSE]
+  } else if (identical(what, "factor")) {
+    mask <- x$fmask
+    dimension <- K
+    vmatrix <- matrix(NA_real_, nrow = prod(dimension), ncol = length(idx))
+
+    vmatrix[, idx <= nv(x)] <- .V(x)[, idx[idx <= nv(x)]]
+    if (any(idx > nv(x))) {
+      # Some factor vectors are not available. Calculate them on-fly.
+      vmatrix[, idx > nv(x)] <- calc.v(x, idx[idx > nv(x)])
+    }
+  }
+
+  if (is.null(mask)) mask <- array(TRUE, dim = dimension)
+
+  # We actually create a reconstruction object with all stuff there..
+  res <- lapply(seq_len(ncol(vmatrix)),
+                function(i) {
+                  vec <- array(NA_real_, dim = dimension)
+                  vec[mask] <- vmatrix[, i]
+
+                  vec
+                })
+
+  # Make and set fake initial series and residuals
+  fakeseries <- array(0, dim = dimension)
+  fakeresiduals <- array(NA_real_, dim = dimension); fakeresiduals[mask] <- 0
+  attr(res, "series") <- fakeseries
+  attr(res, "residuals") <- fakeresiduals
+
+  names(res) <- if (!plot.contrib) idx else paste(idx, " (", .contribution(x, idx, ...), "%)", sep = "")
+
+
+  # Provide convenient defaults
+  dots <- .defaults(dots,
+                    xlab = "",
+                    ylab =  "",
+                    main = if (identical(what, "eigen")) "Eigenvectors" else "Factor vectors",
+                    as.table = TRUE,
+                    scales = list(draw = FALSE, relation = "free"),
+                    aspect = 1,
+                    plot.type = "l",
+                    symmetric = TRUE,
+                    ref = TRUE)
+
+  do.call(plot.nd.ssa.reconstruction, c(list(res,
+                                            slice = slice,
+                                            add.original = FALSE,
+                                            add.residuals = FALSE),
+                                       dots))
+}
