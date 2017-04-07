ca41774bca46d27a24359cfc87f8e619f13a3bc4
Alexander Shlemov <shlemovalex@gmail.com>
2017-04-08 00:08:36 +0300

    Add shaped 2d-ESPRIT test

--- tests/testthat/test-parest.R
+++ tests/testthat/test-parest.R
@@ -119,6 +119,33 @@ test_that("parestimate works correctly for two sines in 2d case", {
   }
 })
 
+test_that("parestimate works correctly for two sines in shaped 2d case", {
+  r <- 4
+  N1 <- 55
+  N2 <- 40
+  T1 <- 5
+  T2 <- 8
+  v1 <- sin(2 * pi * (1:N1) / T1)
+  v2 <- sin(2 * pi * (1:N2) / T2)
+  mx <- outer(v1, v2)
+  ss <- ssa(mx, kind = "2d-ssa", wmask = circle(10))
+  for (solve.method in c("ls", "tls")) {
+    for (pairing.method in c("memp", "diag")) {
+      par <- parestimate(ss, groups = list(sines = 1:4),
+                         solve.method = solve.method,
+                         pairing.method = pairing.method)
+      lm <- par[[1]]$roots
+      mu <- par[[2]]$roots
+      expectred.lm <- exp(pi * 2i / c(T1, -T1, T1, -T1))
+      expectred.mu <- exp(pi * 2i / c(T2, -T2, T2, -T2))
+
+      expect_true(is_multisets_approx_equal(mu, expectred.mu) && is_multisets_approx_equal(lm , expectred.lm),
+                  label = sprintf("Est. ch. roots for sum of sines with periods %3.1f and %3.1f are correct (method = %s-%s)",
+                                  T1, T2, solve.method, pairing.method))
+    }
+  }
+})
+
 test_that("parestimate works correctly for two sines in circular 2d case", {
   r <- 4
   N1 <- 55
