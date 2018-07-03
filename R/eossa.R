540f5fcce080d48ba71b1b22e5e2ea0542dc6918
Alexander Shlemov <shlemovalex@gmail.com>
2018-07-03 16:51:41 +0300

    EOSSA: (c) update

--- R/eossa.R
+++ R/eossa.R
@@ -1,5 +1,5 @@
 #   R package for Singular Spectrum Analysis
-#   Copyright (c) 2017 Alex Shlemov <shlemovalex@gmail.com>
+#   Copyright (c) 2017-2018 Alex Shlemov <shlemovalex@gmail.com>
 #
 #   This program is free software; you can redistribute it
 #   and/or modify it under the terms of the GNU General Public
